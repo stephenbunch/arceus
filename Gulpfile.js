ad97bf4160ad78967f7b19f0e58c5c2d53f59d63
Stephen Bunch <bunchclone@gmail.com>
2015-09-18 20:08:19 -0700

    update gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,7 +10,9 @@ gulp.task( 'make:src', function() {
 });
 
 gulp.task( 'make', function() {
-  return arceus.util.gulpAsync( gulp, 'clean', [ 'make:src' ] );
+  return arceus.util.gulpAsync( gulp, 'clean', [ 'make:src' ] ).then( function() {
+    return arceus.util.touchFileAsync( 'package.json' );
+  });
 });
 
 gulp.task( 'clean', function() {
@@ -18,13 +20,7 @@ gulp.task( 'clean', function() {
 });
 
 gulp.task( 'watch', function() {
-  arceus.util.watch( 'src/**/*', function() {
-    arceus.util.gulpAsync( gulp, 'make:src' ).then( function() {
-      return arceus.util.touchFileAsync( 'package.json' );
-    }).catch( function( err ) {
-      console.log( arceus.util.formatError( err ) );
-    });
-  });
+  arceus.util.gulpWatch( gulp, 'src/**/*', 'make' );
 });
 
 gulp.task( 'default', function() {
